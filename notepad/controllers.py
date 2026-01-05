7bdbf0ccf19de6e76561be8d90b372aca5cdc0a3
archlinux <arch@hotmail.com>
2026-01-04 22:58:12 -0500

    feat(ntp/controller): add logic to connect the signals with file_services module

--- notepad/controllers.py
+++ notepad/controllers.py
@@ -1,8 +1,11 @@
+from PyQt6.QtWidgets import QApplication
+from file_services import FileServices
 class MainController:
     def __init__(self, view):
         self.view = view
-        print(self.view)
+        self.file_services = FileServices(self.view)
         self.view.menu_bar.signal_actions.connect(self._handle_menu_actions)
-
+    
     def _handle_menu_actions(self, action_name):
-        print(action_name)
+        content = self.file_services.open_file()
+        
