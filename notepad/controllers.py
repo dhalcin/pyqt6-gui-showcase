4f3621ae3adca5529eb697d911378bc932847c5e
archlinux <arch@hotmail.com>
2026-01-06 18:30:55 -0500

    feat(ntp/controller): add logic for creating a new window and keep the menu_bar buttons

--- notepad/controllers.py
+++ notepad/controllers.py
@@ -1,11 +1,53 @@
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtCore import Qt
 from file_services import FileServices
+
 class MainController:
-    def __init__(self, view):
+    def __init__(self, view, window_class):
         self.view = view
-        self.file_services = FileServices(self.view)
-        self.view.menu_bar.signal_actions.connect(self._handle_menu_actions)
-    
-    def _handle_menu_actions(self, action_name):
-        content = self.file_services.open_file()
+        self.window_class = window_class
+        self.file_services = FileServices()
+        self.windows = []
+        self._setup_window(self.view)
+        
+    def _setup_window(self, window):
+        window.menu_bar.signal_actions.connect(
+            lambda action_name: self._handle_menu_actions(action_name, window)
+        )
+        
+        #Configurar comportamiento de cierre
+        # Esto le dice a Qt que destruya el objeto C++ al cerrar la ventana
+        window.setAttribute(Qt.WidgetAttribute.WA_DeleteOnClose)
+        
+        #Limpiar nuestra lista de Python cuando la ventana se destruya
+        # Usamos una lambda para saber exactamente qué ventana remover
+        window.destroyed.connect(lambda: self._on_window_destroyed(window))
+
+        self.windows.append(window)
+        window.show()
+
+    def _on_window_destroyed(self, window):
+        if window in self.windows:
+            self.windows.remove(window)
+            print(f"Ventana cerrada. Quedan: {len(self.windows)}")
+
+    def _handle_menu_actions(self, action_name, sender_window):
+        # Al usar sender_window, cada ventana es independiente
+        actions_menu = {
+            #"new_file": "new_file",
+            "new_window": self.open_new_window,
+            "open_file": lambda: self.file_services.open_file(sender_window)
+            # "save",
+            # "save_as",
+            # "print",
+            # "exit"
+        }
+
+        action = actions_menu.get(action_name)
+        if action:
+            action()
+
+    def open_new_window(self):
+        new_window = self.window_class()
+        new_window.setAttribute(Qt.WidgetAttribute.WA_DeleteOnClose)
+        self._setup_window(new_window)
         
