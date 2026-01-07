e5b0b33e02e29d476fe298f8da99ed919b5a85cb
archlinux <arch@hotmail.com>
2026-01-06 21:41:20 -0500

    feat(ntp/file_services): converted the FileServices class to a Stateless Service class

--- notepad/file_services.py
+++ notepad/file_services.py
@@ -2,11 +2,13 @@ from PyQt6.QtWidgets import QFileDialog
 from PyQt6.QtCore import QTextStream, QFile, QIODevice
 
 class FileServices:
-    def __init__(self, main_window):
-        self.main_window = main_window
 
-    def open_file(self):
-        file_path, _= QFileDialog.getOpenFileName(self.main_window, "Open File", "", "Text documents (*.txt);All files(*.*)")
+    # menu bar services
+    def new_file(self):
+        print('selected action new_file')
+
+    def open_file(self, target_window):
+        file_path, _= QFileDialog.getOpenFileName(target_window, "Open File", "", "Text documents (*.txt);All files(*.*)")
         
         if not file_path:
             return
@@ -16,7 +18,7 @@ class FileServices:
         if file.open(QIODevice.OpenModeFlag.ReadOnly | QIODevice.OpenModeFlag.Text):
             try:    
                 stream = QTextStream(file)
-                self.main_window.text_edit.setPlainText(stream.readAll())
+                target_window.text_edit.setPlainText(stream.readAll())
             except Exception as e:
                 print(f"Error processing file : {file.errorString()}")
             
