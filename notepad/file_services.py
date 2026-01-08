1b829cddfde93ae083c2f6629359dfb44fb64cb7
jhon <hankgit-h@hotmail.com>
2026-01-07 21:36:43 -0500

    refactor(ntp/file_services): change the logic of the _write_file method and now use QTextStream to write files

--- notepad/file_services.py
+++ notepad/file_services.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QFileDialog
 from PyQt6.QtCore import QTextStream, QFile, QIODevice
 from pathlib import Path
+import os
 
 class FileServices:
     # menu bar services
@@ -8,7 +9,7 @@ class FileServices:
         print('selected action new_file')
 
     def open_file(self, target_window):
-        file_path, _= QFileDialog.getOpenFileName(target_window, "Open File", "", "Text documents (*.txt);All files(*.*)")
+        file_path, _= QFileDialog.getOpenFileName(target_window, "Open File", "", "Text documents (*.txt);;All Files(*.*)")
         
         if not file_path:
             return
@@ -33,14 +34,17 @@ class FileServices:
 
 
     def _write_file(self, file_path, target_window):
-        try:
-            with open(file_path, 'w', encoding='utf-8') as file:
-                stream = target_window.text_edit.toPlainText()
-                file.write(stream)
-        except FileNotFoundError:
-            print('File not found')
-        except Exception as e:
-            print('error:', e)
+        file = QFile(file_path)
+        if file.open(QIODevice.OpenModeFlag.WriteOnly | QIODevice.OpenModeFlag.Text):
+            try:    
+                stream = QTextStream(file)
+                stream << target_window.text_edit.toPlainText()
+            except Exception as e:
+                print(f"Error processing file : {file.errorString()}")
+            finally:
+                file.close()
+        else:
+            print(f'Error when opening the file : {file.errorString}')    
 
     def save_file(self, target_window):
         file_path = target_window.current_file_path
@@ -49,3 +53,10 @@ class FileServices:
         else:
             path = './new_file.txt'
             self._write_file(path, target_window)
+
+    def save_as_file(self, target_window):
+        file_path, _= QFileDialog.getSaveFileName(target_window, "Save As File", ".txt", "Text documments (*.txt);;All Files(*.*)")
+        if Path(file_path).exists():
+            print('El archivo ya existe')
+        else:
+            print('el archivo no existe, guardando ..')
