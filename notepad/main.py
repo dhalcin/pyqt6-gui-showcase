91dcfeaef26ab2a4865cbd7fb78493e5bf6808ab
archlinux <arch@hotmail.com>
2026-01-14 16:50:34 -0500

    feat(ntp/main): add logic to save content if the window class

--- notepad/main.py
+++ notepad/main.py
@@ -5,6 +5,7 @@ from PyQt6.QtWidgets import (
     QApplication,
     QTextEdit,
     QStatusBar,
+    QMessageBox
 )
 from menu_bar import MenuBar
 from controllers import MainController
@@ -21,6 +22,7 @@ class MainWindow(QMainWindow):
         self.setMenuBar(self.menu_bar)
 
         self.text_edit = QTextEdit()
+        self.text_edit.textChanged.connect(lambda: self.text_edit.document().setModified(True))
 
         self.setCentralWidget(self.text_edit)
 
@@ -28,6 +30,35 @@ class MainWindow(QMainWindow):
         self.setStatusBar(self.status_bar)
 
         self.status_bar.showMessage('notepad', 0)
+
+    def closeEvent(self, event):
+        if self.text_edit.document().isModified():
+            response = QMessageBox.question(
+                self,
+                "Guardar Cambios",
+                "El Documento ha sido modificado. ¿Deseas guardar los cambios?",
+                QMessageBox.StandardButton.Save |
+                QMessageBox.StandardButton.Discard |
+                QMessageBox.StandardButton.Cancel
+            )
+
+            if response == QMessageBox.StandardButton.Save:
+                # We emit the save signal
+                self.menu_bar.signal_actions.emit("save")
+                
+                # We check if it was actually saved, the document should no longer be marked as modified
+                if not self.text_edit.document().isModified():
+                    event.accept()
+                else:
+                    # If the user canceled the "Save As" dialog, we do not close
+                    event.ignore() 
+                    
+            elif response == QMessageBox.StandardButton.Discard:
+                event.accept() # Close without saving
+            else:
+                event.ignore() # Cancel: stays in the window
+        else:
+            event.accept()
 
 def main():
     app = QApplication(sys.argv)
