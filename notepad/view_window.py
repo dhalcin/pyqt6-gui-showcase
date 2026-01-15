006a587b94b2a2ecd84092cec6e3313f0b540f5f
jhon <hankgit-h@hotmail.com>
2026-01-15 18:23:10 -0500

    feat(ntp/view_window): add logic to highlight the found content

--- notepad/view_window.py
+++ notepad/view_window.py
@@ -8,10 +8,19 @@ class View:
             parent=dialog
         )
     
-    def _search_content(self, content_line_edit, target_window):
-        txt = target_window.text_edit.toPlainText()
-        content_to_search = content_line_edit.text()
-        print(txt.find(content_to_search))
+    def _search_content(self, content_line_edit, target_window, dialog):
+        query = content_line_edit.text()
+
+        # al realizar la busqueda se hace tomando en cuenta la posicion del cursor, por lo que found tiende hacer false.
+        # The search takes into account the cursor's position, so `found` tends to return false. I made the cursor start from the beginning.
+        cursor = target_window.text_edit.textCursor()
+        cursor.movePosition(cursor.MoveOperation.Start)
+        target_window.text_edit.setTextCursor(cursor)
+
+        found = target_window.text_edit.find(query)
+        
+        if found:
+            dialog.close()
 
     def find_content(self, target_window):
         dialog = QDialog(target_window)
@@ -22,10 +31,11 @@ class View:
         button = self._create_button('Search', dialog)
         button.setFixedSize(45, 25)
 
-        button.clicked.connect(lambda: self._search_content(line_edit, target_window))
+        button.clicked.connect(lambda: self._search_content(line_edit, target_window, dialog))
 
         layout.addWidget(line_edit)
         layout.addWidget(button)
         dialog.setLayout(layout)
 
         dialog.exec()
+    
