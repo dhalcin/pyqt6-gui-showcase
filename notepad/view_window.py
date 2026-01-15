848e0df54aad70cc184cb0b449aac52278b96676
jhon <hankgit-h@hotmail.com>
2026-01-14 22:57:55 -0500

    feat(ntp/view_window): add logic for creating buttons in the dialog box

--- notepad/view_window.py
+++ notepad/view_window.py
@@ -1,14 +1,24 @@
-from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLineEdit
+from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QPushButton
 
 class View:
+
+    def _create_button(self, text_button, dialog):
+        return QPushButton(
+            text=text_button,
+            parent=dialog
+        )
+
     def find_content(self, target_window):
         dialog = QDialog(target_window)
         dialog.setWindowTitle('Search')
 
         layout = QVBoxLayout()
         line_edit = QLineEdit()
+        button = self._create_button('Search', dialog)
+        button.setFixedSize(45, 25)
 
         layout.addWidget(line_edit)
+        layout.addWidget(button)
         dialog.setLayout(layout)
 
         dialog.exec()
