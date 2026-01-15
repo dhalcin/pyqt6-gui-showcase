05105f02f80b0f51552e654126eaff4fdd1fcc41
jhon <hankgit-h@hotmail.com>
2026-01-14 22:50:19 -0500

    feat(ntp/view_window): add logic to integrate QLineEdit

--- notepad/view_window.py
+++ notepad/view_window.py
@@ -1,7 +1,14 @@
-from PyQt6.QtWidgets import QDialog
+from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLineEdit
 
 class View:
     def find_content(self, target_window):
         dialog = QDialog(target_window)
         dialog.setWindowTitle('Search')
+
+        layout = QVBoxLayout()
+        line_edit = QLineEdit()
+
+        layout.addWidget(line_edit)
+        dialog.setLayout(layout)
+
         dialog.exec()
