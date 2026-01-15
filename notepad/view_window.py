2a546a27d044c0e7db948bcdf20ee53286175c98
jhon <hankgit-h@hotmail.com>
2026-01-14 23:04:08 -0500

    feat(ntp/view_window): add basic logic to search for the requested text

--- notepad/view_window.py
+++ notepad/view_window.py
@@ -7,6 +7,11 @@ class View:
             text=text_button,
             parent=dialog
         )
+    
+    def _search_content(self, content_line_edit, target_window):
+        txt = target_window.text_edit.toPlainText()
+        content_to_search = content_line_edit.text()
+        print(txt.find(content_to_search))
 
     def find_content(self, target_window):
         dialog = QDialog(target_window)
@@ -16,6 +21,8 @@ class View:
         line_edit = QLineEdit()
         button = self._create_button('Search', dialog)
         button.setFixedSize(45, 25)
+
+        button.clicked.connect(lambda: self._search_content(line_edit, target_window))
 
         layout.addWidget(line_edit)
         layout.addWidget(button)
