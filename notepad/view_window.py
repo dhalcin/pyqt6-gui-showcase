c5ee5906edb2bee86f77300a0af0503400db088e
jhon <hankgit-h@hotmail.com>
2026-01-14 22:47:12 -0500

    feat(ntp/view_window): add basic logic for creating a dialog box

--- notepad/view_window.py
+++ notepad/view_window.py
@@ -1,3 +1,7 @@
+from PyQt6.QtWidgets import QDialog
 
 class View:
-    pass
+    def find_content(self, target_window):
+        dialog = QDialog(target_window)
+        dialog.setWindowTitle('Search')
+        dialog.exec()
