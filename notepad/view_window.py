e26723a1f71e548fc13cdbf7e32fe7b19c9950ab
jhon <hankgit-h@hotmail.com>
2026-01-16 23:44:18 -0500

    refactor(ntp/view_widnow): implement dynamic dialog factory for reusable UI components

--- notepad/view_window.py
+++ notepad/view_window.py
@@ -1,17 +1,32 @@
 from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QPushButton
 
 class View:
+    def _dialog(self, target_window, title, text_button, widget=None):
+        dialog = QDialog(target_window)
+        dialog.setWindowTitle(title)
+        layout = QVBoxLayout()
 
-    def _create_button(self, text_button, dialog):
-        return QPushButton(
-            text=text_button,
-            parent=dialog
-        )
-    
+        if widget:
+            layout.addWidget(widget)
+        
+        for text in text_button:
+            button = QPushButton(
+                text=text,
+                parent=dialog
+            )
+
+            button.setFixedSize(45, 25)
+            # Menu find dialog box
+            if text == 'Search':
+                self._button_action(button, lambda: self._search_content(widget, target_window, dialog))  # widget = QLineEdit()
+            layout.addWidget(button)
+        
+        dialog.setLayout(layout)
+        dialog.exec()
+
     def _search_content(self, content_line_edit, target_window, dialog):
         query = content_line_edit.text()
 
-        # al realizar la busqueda se hace tomando en cuenta la posicion del cursor, por lo que found tiende hacer false.
         # The search takes into account the cursor's position, so `found` tends to return false. I made the cursor start from the beginning.
         cursor = target_window.text_edit.textCursor()
         cursor.movePosition(cursor.MoveOperation.Start)
@@ -22,20 +37,8 @@ class View:
         if found:
             dialog.close()
 
+    def _button_action(self, button, action):
+        button.clicked.connect(action)
+
     def find_content(self, target_window):
-        dialog = QDialog(target_window)
-        dialog.setWindowTitle('Search')
-
-        layout = QVBoxLayout()
-        line_edit = QLineEdit()
-        button = self._create_button('Search', dialog)
-        button.setFixedSize(45, 25)
-
-        button.clicked.connect(lambda: self._search_content(line_edit, target_window, dialog))
-
-        layout.addWidget(line_edit)
-        layout.addWidget(button)
-        dialog.setLayout(layout)
-
-        dialog.exec()
-    
+        self._dialog(target_window, 'Search', ['Search'], QLineEdit())
