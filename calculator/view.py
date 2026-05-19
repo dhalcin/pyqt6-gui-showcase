13cd02e703d8298f47487655d657d2bf09f827fb
Jhon <hankgit-h@hotmail.com>
2026-05-18 22:41:52 -0500

    feat(view): add initial display structure with QTextEdit and QLineEdit

--- calculator/view.py
+++ calculator/view.py
@@ -1,4 +1,10 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import (
+    QMainWindow,
+    QVBoxLayout,
+    QWidget,
+    QTextEdit,
+    QLineEdit
+)
 
 class View(QMainWindow):
     def __init__(self):
@@ -7,3 +13,27 @@ class View(QMainWindow):
         self.setWindowTitle("Calculator")
         self.setMinimumSize(330, 500)
 
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        self.main_layout = QVBoxLayout()
+        central_widget.setLayout(self.main_layout)
+
+        # Display
+        self.display = QWidget()
+        self.layout_display = QVBoxLayout()
+
+        self.text_edit = QTextEdit()
+        self.line_edit = QLineEdit()
+
+        self.layout_display.addWidget(self.text_edit)
+        self.layout_display.addWidget(self.line_edit)
+
+        self.display.setLayout(self.layout_display)
+
+        self.main_layout.addWidget(self.display)
+
+
+
+
+        
