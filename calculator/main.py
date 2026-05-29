872a12e66c711777956d7cfbc1017c0be1ed9e80
Jhon <hankgit-h@hotmail.com>
2026-05-28 22:39:25 -0500

    feat: integrate stylesheet loader for custom UI customizatiomization

--- calculator/main.py
+++ calculator/main.py
@@ -1,9 +1,17 @@
 import sys
+import platform
 from PyQt6.QtWidgets import QApplication
 from view import View
         
 def main():
     app = QApplication(sys.argv)
+
+    try: 
+        with open("style.qss", "r") as f:
+            app.setStyleSheet(f.read())
+    except FileNotFoundError:
+        pass
+
     view = View()
     view.show()
     sys.exit(app.exec())
